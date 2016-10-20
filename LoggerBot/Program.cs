427dd98b6508d4c4177a16fdbedddeb36b696218
Noahkiq <noahkiqgaming@gmail.com>
2016-10-20 17:30:30 -0400

    did the actual coding

--- LoggerBot/Program.cs
+++ LoggerBot/Program.cs
@@ -4,13 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Discord;
+using System.IO;
 
 namespace LoggerBot
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) => new Program().Start();
+
+        private DiscordClient _client;
+
+        public void Start()
         {
+            _client = new DiscordClient();
+
+            _client.Log.Message += (s, e) => Console.WriteLine($"[{e.Severity}] {e.Source}: {e.Message}");
+
+            _client.MessageReceived += (s, e) =>
+            {
+                using (StreamWriter text = File.AppendText($"\\logs\\{e.Server.Id}-chatlog.txt"))
+                {
+                    text.WriteLine($"{e.User.Name} ({e.User.Id}) in #{e.Channel.Name} ({e.Channel.Id}) on {e.Server.Name} ({e.Server.Id}): {e.Message.RawText}");    //log msg to txt file
+                    Console.WriteLine($"{e.User.Name} ({e.User.Id}) in #{e.Channel.Name} ({e.Channel.Id}) on {e.Server.Name} ({e.Server.Id}): {e.Message.RawText}"); //log msg to console
+                }
+            };
+
+            string token = File.ReadAllText("token.txt");
+            _client.ExecuteAndWait(async () => {
+                await _client.Connect(token, TokenType.Bot);
+            });
         }
     }
 }
