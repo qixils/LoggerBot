ba4fa79ef6e91816c50d8f36ac4b93f972647962
Noahkiq <noahkiqgaming@gmail.com>
2016-10-20 21:02:34 -0400

    Log message edits

--- LoggerBot/Program.cs
+++ LoggerBot/Program.cs
@@ -29,9 +29,24 @@ namespace LoggerBot
                 File.Create(logpath);*/
                 using (StreamWriter text = File.AppendText($"{e.Server.Id}-chatlog.txt"))
                 {
-                    text.WriteLine($"{e.User.Name} ({e.User.Id}) in #{e.Channel.Name} ({e.Channel.Id}): {e.Message.RawText}");    //log msg to txt file
+                    text.WriteLine($"{e.User.Name} ({e.User.Id}) in #{e.Channel.Name} ({e.Channel.Id}): {e.Message.RawText}");                                       //log msg to txt file
                 }
                 Console.WriteLine($"{e.User.Name} ({e.User.Id}) in #{e.Channel.Name} ({e.Channel.Id}) on {e.Server.Name} ({e.Server.Id}): {e.Message.RawText}");     //log msg to console
+            };
+
+            _client.MessageUpdated += (s, e) =>
+            {
+                /*string mydocuments = $"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}";
+                string path = mydocuments + @"\LoggerBot-Logs";
+                string logpath = $"{path}\\{e.Server.Id}-chatlog.txt";
+                DirectoryInfo di = Directory.CreateDirectory(path);
+                File.Create(logpath);*/
+                using (StreamWriter text = File.AppendText($"{e.Server.Id}-chatlog.txt"))
+                {
+                    text.WriteLine($"{e.User.Name} ({e.User.Id}) edited their message in #{e.Channel.Name} ({e.Channel.Id}) | Before: - {e.Before.RawText} | After: - {e.After.RawText}");                                       //log msg to txt file
+
+                }
+                Console.WriteLine($"{e.User.Name} ({e.User.Id}) edited their message in #{e.Channel.Name} ({e.Channel.Id}) on {e.Server.Name} ({e.Server.Id}) | Before: - {e.Before.RawText} | After: - {e.After.RawText}");     //log msg to console
             };
 
             string token = File.ReadAllText("token.txt");
